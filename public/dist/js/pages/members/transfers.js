b32bb5140596a5cf6fb29955317f6a310760e9df
ncmackenzie <34639621+nckmackenzie@users.noreply.github.com>
2022-10-03 14:01:15 +0300

    form validation and submission

--- public/dist/js/pages/members/transfers.js
+++ public/dist/js/pages/members/transfers.js
@@ -13,7 +13,9 @@ const currDistrictSelect = document.getElementById('district');
 const memberSelect = document.getElementById('member');
 const newCongSelect = document.getElementById('newcongregation');
 const newDistrictSelect = document.getElementById('newdistrict');
-const saveBtn = document.getElementById('save');
+const form = document.getElementById('transferForm');
+const dateInput = document.getElementById('date');
+const controls = document.querySelectorAll('.form-control');
 
 currCongSelect.addEventListener('change', async function (e) {
   if (!e.target.value || e.target.value === '') return;
@@ -33,4 +35,44 @@ currDistrictSelect.addEventListener('change', async function (e) {
   memberSelect.innerHTML = await getMembers(e.target.value);
 
   activateMultiSelect();
+});
+
+controls.forEach(control => {
+  control.addEventListener('change', function () {
+    control.classList.remove('is-invalid');
+    control.nextSibling.nextSibling.textContent = '';
+  });
+});
+
+function validate() {
+  let errorCount = 0;
+
+  controls.forEach(contrl => {
+    if (!contrl.value || contrl.value === '') {
+      contrl.classList.add('is-invalid');
+      contrl.nextSibling.nextSibling.textContent = 'This field is required';
+      errorCount++;
+    }
+  });
+
+  if (+newDistrictSelect.value === +currDistrictSelect.value) {
+    newCongSelect.classList.add('is-invalid');
+    newCongSelect.nextSibling.nextSibling.textContent =
+      'Old and new districts cannot be same';
+    errorCount++;
+  }
+
+  if (new Date(dateInput.value).getTime() > new Date().getTime()) {
+    dateInput.classList.add('is-invalid');
+    dateInput.nextSibling.nextSibling.textContent = 'Invalid transfer date';
+  }
+
+  if (errorCount > 0) return false;
+  return true;
+}
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+  if (!validate()) return;
+  document.transferForm.submit();
 });
