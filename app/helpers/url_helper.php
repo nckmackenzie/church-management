67a38565637ff801c961723f6d15568519d72c31
ncmackenzie <34639621+nckmackenzie@users.noreply.github.com>
2022-10-04 13:23:49 +0300

    @fix change from CenterId to congregationId

--- app/helpers/url_helper.php
+++ app/helpers/url_helper.php
@@ -270,3 +270,39 @@ function checkcenter($cong){
         exit();
     }
 }
+
+//get unique no from database;
+function getuniqueid($con,$field,$table,$cid,$bycenter = true){
+    $sql = "SELECT COUNT(*) FROM $table WHERE Deleted = 0";
+    if($bycenter){
+        $sql .= " AND (congregationId = :cid)";
+    }
+    $stmt = $con->prepare($sql);
+    if($bycenter){
+        $stmt->bindValue(':cid',$cid);
+    }
+    $stmt->execute();
+    if((int)$stmt->fetchColumn() === 0){
+        return 1;
+    }else{
+        $sql = "SELECT 
+                    $field 
+                FROM 
+                    $table 
+                WHERE 
+                    deleted = 0";
+        if($bycenter){
+            $sql .= " AND (congregationId = :cid)";
+        }
+        $sql .=" ORDER BY $field DESC";
+        $stmt = $con->prepare($sql);
+        if($bycenter){
+            $stmt->bindValue(':cid',$cid);
+        }
+        if($stmt->execute()){
+            return (int)$stmt->fetchColumn() + 1;
+        }else{
+            return 0;
+        }
+    }
+}
