e7205e15e77fa74248b70f64dcea7bd6a07a9976
ncmackenzie <34639621+nckmackenzie@users.noreply.github.com>
2022-10-05 14:58:05 +0300

    change flash name

--- app/views/groupfunds/index.php
+++ app/views/groupfunds/index.php
@@ -32,7 +32,7 @@
     <!-- Content Header (Page header) -->
     <section class="content-header">
       <div class="container-fluid">
-        <?php flash('product_msg');?>
+        <?php flash('request_msg');?>
         <div class="row mb-2">
           <div class="col-sm-6">
             <a href="<?php echo URLROOT;?>/groupfunds/add" class="btn btn-sm btn-success custom-font">Add New</a>
