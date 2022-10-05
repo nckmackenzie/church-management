d12364e261f1f374b92a21c5cff5871ff45a9a00
ncmackenzie <34639621+nckmackenzie@users.noreply.github.com>
2022-10-05 11:36:15 +0300

    create a global send http request function

--- public/dist/js/pages/utils/utils.js
+++ public/dist/js/pages/utils/utils.js
@@ -1,1 +1,24 @@
 export const HOST_URL = 'http://localhost/cms';
+
+//function to make http requests
+export async function sendHttpRequest(
+  url,
+  method = 'GET',
+  body = null,
+  headers = {}
+) {
+  try {
+    const res = await fetch(url, {
+      method,
+      body,
+      headers,
+    });
+
+    const data = await res.json();
+    if (!res.ok) throw new Error(data.message);
+    return data;
+  } catch (error) {
+    alert('There was a problem executing this command! Contact admin for help');
+    console.error(error.message);
+  }
+}
