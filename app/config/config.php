0e4e6ddbac3e7d691723735cead324343a4bfa5a
nckmackenzie <nicholas.mackenzie1@gmail.com>
2022-10-08 14:21:20 +0300

    change db name and set const for environment var

--- app/config/config.php
+++ app/config/config.php
@@ -3,10 +3,11 @@
 define('DB_HOST','localhost');
 define('DB_USER','root');
 define('DB_PASS','');
-define('DB_NAME','pceakalimoni');
+define('DB_NAME','cms');
 //APP ROOT
 define('APPROOT',dirname(dirname(__FILE__)));
 //url root
 define('URLROOT','http://localhost/cms');
 //SITE NAME
 define('SITENAME','Kenyatta Rd Parish');
+define('ENVIRONMENT','production');
