e9e2a666ee7397510390e15ea009eccdfc03348c
nckmackenzie <nicholas.mackenzie1@gmail.com>
2022-10-08 16:41:03 +0300

    add more reusable methods

--- app/models/Reusables.php
+++ app/models/Reusables.php
@@ -13,4 +13,28 @@ class Reusables
         $parentid = getdbvalue($this->db->dbh,$sql,[$childgl]);
         return trim(getdbvalue($this->db->dbh,'SELECT accountType FROM tblaccounttypes WHERE (ID = ?)',[$parentid]));
     }
+
+    public function GetAccounts($id)
+    {
+        $this->db->query('SELECT ID,UCASE(accountType) AS accountType FROM tblaccounttypes 
+                          WHERE (accountTypeId = :id) AND (deleted=0) AND (isBank = 0) AND (parentId <> 0) AND (isSubCategory = 1) ORDER BY accountType');
+        $this->db->bind(':id',$id);
+        return $this->db->resultSet();                  
+    }
+
+    public function PaymentMethods()
+    {
+       return paymentMethods($this->db->dbh);
+    }
+
+    public function GetBanks()
+    {
+        $sql = "SELECT ID,UCASE(CONCAT(accountType,'-',IFNULL(accountNo,''))) AS Bank FROM tblaccounttypes WHERE isBank = 1 AND CongregationId = ?";
+        return loadresultset($this->db->dbh,$sql,[$_SESSION['congId']]);
+    }
+
+    public function CheckYearClosed($yearid)
+    {
+        return yearprotection($this->db->dbh,$yearid);
+    }
 }
