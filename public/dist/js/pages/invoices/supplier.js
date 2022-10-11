74ed15789b0c055cd7e14c4305f98d0eb905bc9b
nckmackenzie <nicholas.mackenzie1@gmail.com>
2022-10-11 20:12:23 +0300

    remove product from table on remove button click

--- public/dist/js/pages/invoices/supplier.js
+++ public/dist/js/pages/invoices/supplier.js
@@ -1,6 +1,20 @@
-import { mandatoryFields, clearOnChange } from '../utils/utils.js';
-import { getSupplierDetails, saveProduct } from './ajax-requests.js';
-import { formatDate, addDays, validateModal } from './functionalities.js';
+import {
+  mandatoryFields,
+  clearOnChange,
+  updateSubTotal,
+} from '../utils/utils.js';
+import {
+  getSupplierDetails,
+  saveProduct,
+  getProductRate,
+} from './ajax-requests.js';
+import {
+  formatDate,
+  addDays,
+  validateModal,
+  calcGrossValue,
+  addToTable,
+} from './functionalities.js';
 
 const supplierSelect = document.getElementById('supplier');
 const invoiceDateInput = document.getElementById('idate');
@@ -10,11 +24,20 @@ const pinInput = document.getElementById('pin');
 const emailInput = document.getElementById('email');
 const vatTypeSelect = document.getElementById('vattype');
 const vatSelect = document.getElementById('vat');
-const productSelect = document.getElementById('product');
+export const productSelect = document.getElementById('product');
 const modalForm = document.getElementById('productForm');
 const modalBtn = document.getElementById('newproduct');
 export const modalRequired = document.querySelectorAll('.modalrequired');
 export const modalAlert = document.querySelector('.modalAlert');
+export const rateInput = document.getElementById('rate');
+export const qtyInput = document.getElementById('qty');
+export const grossInput = document.getElementById('gross');
+const addBtn = document.getElementById('add');
+const totalsInput = document.getElementById('totals');
+export const table = document.getElementById('details');
+const addInputControls = document.querySelectorAll('.addcontrol');
+const invoiceForm = document.getElementById('invoiceForm');
+// export const descriptionInput = document.getElementById('description');
 
 //fetch supplier details
 supplierSelect.addEventListener('change', async function (e) {
@@ -40,12 +63,17 @@ vatTypeSelect.addEventListener('change', function (e) {
   }
 });
 
-productSelect.addEventListener('change', function (e) {
-  console.log(e.target.value);
+productSelect.addEventListener('change', async function (e) {
   if (e.target.value == '' || !e.target.value) return;
   if (+e.target.value === 0) {
     $('#addModal').modal('show');
     this.value = '';
+    return;
+  }
+  rateInput.value = await getProductRate(+e.target.value);
+  if (rateInput.value != '') {
+    rateInput.classList.remove('is-invalid');
+    rateInput.nextSibling.nextSibling.textContent = '';
   }
 });
 
@@ -67,7 +95,6 @@ modalForm.addEventListener('submit', async function (e) {
   modalBtn.disabled = false;
   modalBtn.textContent = 'Save';
   if (data) {
-    // console.log(data);
     document.getElementById('name').value =
       document.getElementById('desc').value =
       document.getElementById('sellingprice').value =
@@ -77,9 +104,40 @@ modalForm.addEventListener('submit', async function (e) {
     productSelect.innerHTML = '';
     productSelect.innerHTML = data.products;
     productSelect.value = data.productid;
-    document.getElementById('rate').value = rate;
+    rateInput.value = rate;
   }
 });
 
+qtyInput.addEventListener('blur', calcGrossValue);
+rateInput.addEventListener('blur', calcGrossValue);
+
+addBtn.addEventListener('click', function () {
+  let error = 0;
+  addInputControls.forEach(cntrl => {
+    if (cntrl.value === '') {
+      cntrl.classList.add('is-invalid');
+      cntrl.nextSibling.nextSibling.textContent = 'Field is required';
+      error++;
+    }
+  });
+  if (error > 0) return;
+  addToTable();
+  updateSubTotal(table, 4, 'input', totalsInput);
+  productSelect.value =
+    qtyInput.value =
+    rateInput.value =
+    grossInput.value =
+      '';
+});
+
+//remove button clicked
+table.addEventListener('click', function (e) {
+  if (!e.target.classList.contains('text-danger')) return;
+  const btn = e.target;
+  btn.closest('tr').remove();
+  updateSubTotal(table, 4, 'input', totalsInput);
+});
+
 clearOnChange(mandatoryFields);
+clearOnChange(addInputControls);
 vatSelect.value = '';
