bd2e54dd25277c8d11c7db6f60ee16cc8da26398
nckmackenzie <nicholas.mackenzie1@gmail.com>
2022-10-11 22:34:26 +0300

    add total to header object

--- public/dist/js/pages/invoices/functionalities.js
+++ public/dist/js/pages/invoices/functionalities.js
@@ -97,6 +97,7 @@ export function header() {
     invoiceNo: invoiceNoInput.value,
     id: idInput.value,
     isEditInput: isEditInput.value,
+    total: numberFormatter(totalsInput.value),
   };
 }
 
