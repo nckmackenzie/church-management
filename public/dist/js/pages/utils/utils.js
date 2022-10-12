51ee1a4941cffd72288fa5e0710ca4ccf397fd53
nckmackenzie <nicholas.mackenzie1@gmail.com>
2022-10-12 20:58:23 +0300

    add function to format date to ddMMyy

--- public/dist/js/pages/utils/utils.js
+++ public/dist/js/pages/utils/utils.js
@@ -126,3 +126,16 @@ export function validateDate(start, end) {
   }
   return true;
 }
+
+export function formatDate(date) {
+  const providedDate = new Date(date);
+  const yyyy = providedDate.getFullYear();
+  let mm = providedDate.getMonth() + 1; // Months start at 0!
+  let dd = providedDate.getDate();
+
+  if (dd < 10) dd = '0' + dd;
+  if (mm < 10) mm = '0' + mm;
+
+  const formattedToday = dd + '/' + mm + '/' + yyyy;
+  return formattedToday;
+}
