3cdfb3fe39e9dd0498f3ff4695b9e5cdc1c6e643
nckmackenzie <nicholas.mackenzie1@gmail.com>
2022-10-13 20:56:44 +0300

    http request to get all invoice payments

--- public/dist/js/pages/reports/ajax.js
+++ public/dist/js/pages/reports/ajax.js
@@ -5,16 +5,20 @@ export async function invoiceReports(type,criteria=null,sdate=null,edate=null) {
     let url;
     if(type === 'balances'){
         url = `${HOST_URL}/invoicereports/getinvoicereport?type=${type}`
-    }else if(type ='byinvoice'){
+    }else if(type === 'byinvoice'){
       url = `${HOST_URL}/invoicereports/getinvoicereport?type=${type}&criteria=${criteria}`
+    }else if(type === 'bysupplier'){
+      url = `${HOST_URL}/invoicereports/getinvoicereport?type=${type}&criteria=${criteria}&sdate=${sdate}&edate=${edate}`;
+    }else if(type === 'all'){
+      url = `${HOST_URL}/invoicereports/getinvoicereport?type=${type}&sdate=${sdate}&edate=${edate}`;
     }
     const res = await sendHttpRequest(url,'GET',undefined,{},alertBox);
     return await res
 }
 
-export async function getInvoiceNo() {
+export async function getSelectOptions(type) {
   const res = await sendHttpRequest(
-    `${HOST_URL}/invoicereports/fetchinvoicenos`,
+    `${HOST_URL}/invoicereports/fetchselectoptions?type=${type}`,
     'GET',
     undefined,
     {},
