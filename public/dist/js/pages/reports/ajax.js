17e47ac04a0631660acf2587586c7b9536ef9e1e
ncmackenzie <34639621+nckmackenzie@users.noreply.github.com>
2022-10-13 16:06:56 +0300

    ajax call to get invoice nos

--- public/dist/js/pages/reports/ajax.js
+++ public/dist/js/pages/reports/ajax.js
@@ -9,3 +9,15 @@ export async function invoiceReports(type,sdate=null,edate=null,criteria=null) {
     const res = await sendHttpRequest(url,'GET',undefined,{},alertBox);
     return await res
 }
+
+export async function getInvoiceNo() {
+  const res = await sendHttpRequest(
+    `${HOST_URL}/invoicereports/fetchinvoicenos`,
+    'GET',
+    undefined,
+    {},
+    alertBox
+  );
+
+  return res;
+}
