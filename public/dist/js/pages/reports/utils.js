032984e4e679a75ceb3e92ec8dea2f90d2a738a9
ncmackenzie <34639621+nckmackenzie@users.noreply.github.com>
2022-10-13 15:38:59 +0300

    export basic form controls for reports

---
+++ public/dist/js/pages/reports/utils.js
@@ -0,0 +1,5 @@
+export const btnPreview = document.querySelector('.preview');
+export const sdateInput = document.querySelector('#sdate');
+export const edateInput = document.querySelector('#edate');
+export const resultsDiv = document.querySelector('#results');
+export const reportTypeSelect = document.querySelector('#type');
