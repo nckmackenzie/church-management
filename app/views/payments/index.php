81863d6bb30b76fb247a4fae69804e69a3eaa5ce
ncmackenzie <34639621+nckmackenzie@users.noreply.github.com>
2022-10-13 10:05:11 +0300

    table layout and details changes

--- app/views/payments/index.php
+++ app/views/payments/index.php
@@ -1,33 +1,7 @@
 <?php require APPROOT . '/views/inc/header.php';?>
 <?php require APPROOT . '/views/inc/topNav.php';?>
 <?php require APPROOT . '/views/inc/sideNav.php';?>
-<div class="modal fade" id="deleteModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-centered" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLongTitle">Delete Deposit</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-          <form action="<?php echo URLROOT;?>/deposits/delete" method="post">
-              <div class="row">
-                <div class="col-md-9">
-                  <label for="">Are You Sure You Want To Delete Selected deposit?</label>
-                  <input type="hidden" name="id" id="id">
-                </div>
-              </div>
-              <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                <button type="submit" class="btn btn-danger">Yes</button>
-              </div>
-          </form>
-      </div>
-     
-    </div>
-  </div>
-</div>
+
 <div class="content-wrapper">
     <!-- Content Header (Page header) -->
     <section class="content-header">
@@ -44,46 +18,42 @@
     <section class="content">
         <div class="row">
             <div class="col-md-12 table-responsive">
-                <table class="table table-striped table-bordered table-sm" id="depositsTable">
+                <table class="table table-striped table-bordered table-sm" id="paymentsTable">
                     <thead class="bg-navy">
-                        <th class="d-none">ID</th>
-                        <th>Deposited Date</th>
-                        <th>Bank</th>
-                        <th>Amount</th>
-                        <th>Reference</th>
+                        <th>Payment No</th>
+                        <th>Supplier</th>
+                        <th>Payment Date</th>
+                        <th>Amount Paid</th>
                         <th>Actions</th>
                     </thead>
                     <tbody>
-                       
+                       <?php foreach($data['payments'] as $payment): ?>
+                          <tr>
+                            <td><?php echo $payment->paymentNo;?></td>
+                            <td><?php echo $payment->supplierName;?></td>
+                            <td><?php echo date('d-m-Y',strtotime($payment->paymentDate)) ;?></td>
+                            <td><?php echo number_format($payment->AmountPaid,2);?></td>
+                            <td>
+                              <?php $route = URLROOT .'/payments/print?paymentno='.$payment->paymentNo .'&supplier='.$payment->supplierId?>
+                              <a href="<?php echo URLROOT;?>/payments/print/<?php echo $payment->paymentNo;?>-<?php echo $payment->supplierId;?>" target="_blank" class="btn btn-sm bg-olive custom-font">Print</a>
+                            </td>
+                          </tr>
+                       <?php endforeach; ?>
                     </tbody>
                 </table>
             </div>    
         </div>        
     </section><!-- /.content -->
 </div><!-- /.content-wrapper -->
-
-
-
 <?php require APPROOT . '/views/inc/footer.php'?>
 <script>
-    $(function(){
-      $('#depositsTable').DataTable({
+  $('#paymentsTable').DataTable({
           'ordering' : false,
           'columnDefs' : [
-            {"width" : "10%" , "targets": 5},
+            {"width" : "15%" , "targets": 0},
+            {"width" : "15%" , "targets": 4},
           ]
       });
-
-      $('#depositsTable').on('click','.btndel',function(){
-          $('#deleteModalCenter').modal('show');
-          $tr = $(this).closest('tr');
-
-          let data = $tr.children('td').map(function(){
-              return $(this).text();
-          }).get();
-          $('#id').val(data[0]);
-      });
-    });
 </script>
 </body>
 </html>
