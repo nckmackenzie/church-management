a25d5eca6215c09fb80c2bfbfd2ea618e75af148
ncmackenzie <34639621+nckmackenzie@users.noreply.github.com>
2022-10-14 14:50:29 +0300

    functionality for detailed tb

---
+++ public/dist/js/pages/reports/tb/tb.js
@@ -0,0 +1,55 @@
+//prettier-ignore
+import {mandatoryFields,clearOnChange,validation,validateDate,numberWithCommas,getColumnTotal} from '../../utils/utils.js'
+import {
+  btnPreview,
+  sdateInput,
+  edateInput,
+  reportTypeSelect,
+  resultsDiv,
+  setLoadingSpinner,
+  removeLoadingSpinner,
+  createSpinnerContainer,
+} from '../utils.js';
+import { getTrialBalance } from '../ajax.js';
+const table = document.getElementById('table');
+
+//click handler
+btnPreview.addEventListener('click', async function () {
+  if (validation() > 0) return;
+  if (!validateDate(sdateInput, edateInput)) return;
+  resultsDiv.classList.add('d-none');
+  setLoadingSpinner();
+  const sdateVal = sdateInput.value;
+  const edateVal = edateInput.value;
+  const reportType = reportTypeSelect.value;
+  const data = await getTrialBalance(reportType, sdateVal, edateVal);
+  removeLoadingSpinner(resultsDiv);
+  if (data && data.success) {
+    appendTbody(data.results);
+    const debitTotal = document.getElementById('debittotal');
+    const creditTotal = document.getElementById('credittotal');
+    debitTotal.innerText = getColumnTotal(table, 1);
+    creditTotal.innerText = getColumnTotal(table, 2);
+  }
+});
+
+function appendTbody(data) {
+  const tbody = table.getElementsByTagName('tbody')[0];
+  data.forEach(dt => {
+    let html = `
+        <tr>
+            <td>${String(dt.account).toUpperCase()}</td>
+            <td class="text-center">${numberWithCommas(dt.Debit)}</td>
+            <td class="text-center">${
+              isNaN(parseFloat(dt.credit))
+                ? ''
+                : numberWithCommas(parseFloat(dt.credit).toFixed(2))
+            }</td>
+        </tr>
+    `;
+    tbody.insertAdjacentHTML('beforeend', html);
+  });
+}
+
+clearOnChange(mandatoryFields);
+createSpinnerContainer();
