d6f598835799d14efbd96c4be5e929a46593bab6
ncmackenzie <34639621+nckmackenzie@users.noreply.github.com>
2022-10-14 08:42:38 +0300

    add reusable spinner functionalities

--- public/dist/js/pages/reports/utils.js
+++ public/dist/js/pages/reports/utils.js
@@ -3,3 +3,22 @@ export const sdateInput = document.querySelector('#sdate');
 export const edateInput = document.querySelector('#edate');
 export const resultsDiv = document.querySelector('#results');
 export const reportTypeSelect = document.querySelector('#type');
+const contentDiv = document.querySelector('.content');
+
+export function createSpinnerContainer() {
+  contentDiv.insertAdjacentHTML(
+    'afterbegin',
+    '<div class="spinner-container d-flex justify-content-center"></div>'
+  );
+}
+
+export function setLoadingSpinner() {
+  const spinnerContainer = document.querySelector('.spinner-container');
+  let html = `<div class="spinner md"></div> `;
+  spinnerContainer.innerHTML = html;
+}
+
+export function removeLoadingSpinner() {
+  const spinnerContainer = document.querySelector('.spinner-container');
+  spinnerContainer.innerHTML = '';
+}
