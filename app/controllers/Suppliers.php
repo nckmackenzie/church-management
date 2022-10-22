25d5b91bcc533b06ff2afbdcd82946d7dadfffeb
nckmackenzie <nicholas.mackenzie1@gmail.com>
2022-10-22 19:02:05 +0300

    save functionality

--- app/controllers/Suppliers.php
+++ app/controllers/Suppliers.php
@@ -41,4 +41,54 @@ class Suppliers extends Controller
         $this->view('suppliers/add',$data);
         exit;
     }
+
+    //create or update supplier method
+    public function createupdate()
+    {
+        if($_SERVER['REQUEST_METHOD'] === 'POST')
+        {
+            $fields = json_decode(file_get_contents('php://input')); //extract submited json data
+            $data = [
+                'id' => (int)trim($fields->id),
+                'isedit' => converttobool(trim($fields->isedit)),
+                'suppliername' => !empty($fields->suppliername) ? strtolower(trim($fields->suppliername)) : null,
+                'contact' => !empty($fields->contact) ? trim($fields->contact) : null,
+                'pin' => !empty($fields->pin) ? trim($fields->pin) : null,
+                'address' => !empty($fields->address) ? strtolower(trim($fields->address)) : null,
+                'email' => !empty($fields->email) ? strtolower(trim($fields->email)) : null,
+                'contactperson' => !empty($fields->contactperson) ? strtolower(trim($fields->contactperson)) : null,
+                'balance' => converttobool($fields->isedit) ? null : (!empty($fields->openingbal) ? trim($fields->openingbal) : null),
+                'asof' => converttobool($fields->isedit) ? null : (!empty($fields->asof) ? date('Y-m-d',strtotime(trim($fields->asof))) : null)
+            ];
+            //validate date
+            if(is_null($data['suppliername'])){
+                http_response_code(400);
+                echo json_encode(['message' => 'Enter supplier name']);
+                exit;
+            }
+            if(!is_null($data['balance']) && is_null($data['asof'])){
+                http_response_code(400);
+                echo json_encode(['message' => 'Enter date of balance']);
+                exit;
+            }
+            if($data['asof'] > date('Y-m-d')){
+                http_response_code(400);
+                echo json_encode(['message' => 'Invalid balance date selected']);
+                exit;
+            }
+            //if was unable to save
+            if(!$this->suppliermodel->CreateUpdate($data)){
+                http_response_code(400);
+                echo json_encode(['message' => 'Unable to save supplier. Retry or contact admin']);
+                exit;
+            }
+            //success
+            echo json_encode(['success' => true]);
+            exit;
+
+        }else{
+            redirect('users/deniedaccess');
+            exit;
+        }
+    }
 }
--- app/models/Supplier.php
+++ app/models/Supplier.php
@@ -12,4 +12,60 @@ class Supplier
     {
         return loadresultset($this->db->dbh,'SELECT * FROM vw_suppliers WHERE congregationId = ?',[(int)$_SESSION['congId']]);
     }
+
+    public function CreateUpdate($data)
+    {
+        try {
+            $this->db->dbh->beginTransaction();
+
+            if($data['isedit']){
+
+            }else{
+                $this->db->query('INSERT INTO tblsuppliers (supplierName,contact,`address`,pin,email,contactPerson,congregationId) 
+                                  VALUES(:sname,:contact,:add,:pin,:email,:cperson,:cid)');
+                $this->db->bind(':sname',$data['suppliername']);
+                $this->db->bind(':contact',$data['contact']);
+                $this->db->bind(':add',$data['address']);
+                $this->db->bind(':pin',$data['pin']);
+                $this->db->bind(':email',$data['email']);
+                $this->db->bind(':cperson',$data['contactperson']);
+                $this->db->bind(':cid',$_SESSION['congId']);
+                $this->db->execute();
+                $tid = $this->db->dbh->lastInsertId();
+                
+
+                if(floatval($data['balance'])){
+                    $yearid = getYearId($this->db->dbh, $data['asof']);
+                    $this->db->query('INSERT INTO tblinvoice_header_suppliers (invoiceDate,supplierId,
+                                                  fiscalYearId,inclusiveVat,postedBy,congregationId)
+                                      VALUES(:idate,:cid,:fid,:ivat,:pby,:cong)');
+                    $this->db->bind(':idate',$data['asof']);
+                    $this->db->bind(':cid',$tid);
+                    $this->db->bind(':fid',$yearid);
+                    $this->db->bind(':ivat',$data['balance']);
+                    $this->db->bind(':pby',$_SESSION['userId']);
+                    $this->db->bind(':cong',$_SESSION['congId']);
+                    $this->db->execute();
+
+                    saveToLedger($this->db->dbh,$data['asof'],'accounts payable','payables and accruals',0,$data['balance']
+                            ,'supplier opening balance',4,15,$tid,$_SESSION['congId']);
+                    saveToLedger($this->db->dbh,$data['asof'],'uncategorized expenses','uncategorized expenses',$data['balance'],0
+                            ,'supplier opening balance',2,15,$tid,$_SESSION['congId']);
+                }
+
+                if(!$this->db->dbh->commit()){
+                    return false;
+                }else{
+                    return true;
+                }
+            }
+
+        } catch (PDOException $e) {
+            if($this->db->dbh->inTransaction()){
+                $this->db->dbh->rollBack();
+            }
+            error_log($e->getMessage(),0);
+            return false;
+        }
+    }
 }
