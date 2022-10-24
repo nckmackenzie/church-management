0d44d0f5bd7b69b7cf978f5deea636bd8bdbeefa
ncmackenzie <34639621+nckmackenzie@users.noreply.github.com>
2022-10-24 13:55:20 +0300

    add ability for super users to change congregation

--- app/views/mains/index.php
+++ app/views/mains/index.php
@@ -5,10 +5,30 @@
   <div class="content-wrapper">
     <!-- Main content -->
     <section class="content">
-        
+      <div class="row">
+        <div class="col-12 mt-2">
+          <?php flash('main_msg');?>
+        </div>
+      </div>
+      <?php if((int)$_SESSION['userType'] === 1 || (int)$_SESSION['userType'] === 6) : ?> 
+        <div class="row">
+          <div class="col-md-4 mt-4">
+            <form action="<?php echo URLROOT;?>/mains/changecongregation" method="post">
+                <div class="form-group">
+                  <select name="congregation" id="congregation" class="form-control form-control-sm">
+                      <option value="" selected disabled>Select congregation to change to...</option>
+                      <?php foreach($data['congregations'] as $congregation) : ?>
+                        <option value="<?php echo $congregation->ID;?>"><?php echo strtoupper($congregation->CongregationName);?></option>
+                      <?php endforeach; ?>
+                  </select>
+                </div>
+                <button class="btn btn-sm bg-navy">Change</button>
+            </form>
+          </div>
+        </div>
+      <?php endif; ?>
     </section><!-- /.content -->
 </div><!-- /.content-wrapper -->
-  
 <?php require APPROOT . '/views/inc/footer.php'?>
 </body>
 </html>
