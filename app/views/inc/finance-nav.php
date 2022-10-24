c4f264e6dbe1f95299ca7b8db978a352965da611
ncmackenzie <34639621+nckmackenzie@users.noreply.github.com>
2022-10-24 08:59:52 +0300

    finance admin nav for congregations

--- app/views/inc/finance-nav.php
+++ app/views/inc/finance-nav.php
@@ -2,50 +2,96 @@
     <ul class="nav nav-pills nav-sidebar flex-column nav-child-indent"
         data-widget="treeview" role="menu" data-accordion="false">
         <li class="nav-item">
-          <a href="#" class="nav-link">
-              <i class="nav-icon fas fa-money-check-alt"></i>
-              <p class="custom-bold custom-font">
-              FINANCE
-              <i class="right fas fa-angle-left"></i>
-              </p>
-          </a>
-          <ul class="nav nav-treeview custom-font">
-            <li class="nav-item">
-                <a href="<?php echo URLROOT;?>/banks" class="nav-link">
-                    <p>Banks</p>
-                </a>
-            </li>
-            <li class="nav-item">
-                <a href="<?php echo URLROOT;?>/contributions" class="nav-link">
-                    <p>Receipts</p>
-                </a>
-            </li>
-            <li class="nav-item">
-                <a href="<?php echo URLROOT;?>/expenses" class="nav-link">
-                    <p>Expenses</p>
-                </a>
-            </li>
-            <li class="nav-item">
-                <a href="<?php echo URLROOT;?>/pledges" class="nav-link">
-                    <p>Pledges</p>
-                </a>
-            </li>
-            <li class="nav-item">
-                <a href="<?php echo URLROOT;?>/churchbudgets" class="nav-link">
-                    <p>Church Budget</p>
-                </a>
-            </li>
-            <li class="nav-item">
-                <a href="<?php echo URLROOT;?>/groupbudgets" class="nav-link">
-                    <p>Group Budget</p>
-                </a>
-            </li>
-            <li class="nav-item">
-                <a href="<?php echo URLROOT;?>/journals" class="nav-link">
-                    <p>Journal Entry</p>
-                </a>
-            </li>
-            <li class="nav-item">
+            <a href="#" class="nav-link">
+                <i class="nav-icon fas fa-cogs"></i>
+                <p class="custom-bold custom-font">
+                MASTER ENTRY
+                <i class="right fas fa-angle-left"></i>
+                </p>
+            </a>
+            <ul class="nav nav-treeview custom-font">
+                <li class="nav-item">
+                    <a href="<?php echo URLROOT;?>/customers" class="nav-link">
+                        <p>Customers</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="<?php echo URLROOT;?>/suppliers" class="nav-link">
+                        <p>Suppliers</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="<?php echo URLROOT;?>/products" class="nav-link">
+                        <p>Products</p>
+                    </a>
+                </li>
+            </ul>
+        </li>
+        <li class="nav-item">
+            <a href="#" class="nav-link">
+                <i class="nav-icon fas fa-money-check-alt"></i>
+                <p class="custom-bold custom-font">
+                FINANCE
+                <i class="right fas fa-angle-left"></i>
+                </p>
+            </a>
+            <ul class="nav nav-treeview custom-font">
+                <li class="nav-item">
+                    <a href="<?php echo URLROOT;?>/banks" class="nav-link">
+                        <p>Banks</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="<?php echo URLROOT;?>/contributions" class="nav-link">
+                        <p>Receipts</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="<?php echo URLROOT;?>/mmfreceipts" class="nav-link">
+                        <p>MMF Receipts</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="<?php echo URLROOT;?>/groupfunds" class="nav-link">
+                        <p>Group funds requisition</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="<?php echo URLROOT;?>/groupfunds/approvals" class="nav-link">
+                        <p>Group funds approval</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="<?php echo URLROOT;?>/expenses" class="nav-link">
+                        <p>Expenses</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="<?php echo URLROOT;?>/pledges" class="nav-link">
+                        <p>Pledges</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="<?php echo URLROOT;?>/cashreceipts" class="nav-link">
+                        <p>Petty Cash Receipt</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="<?php echo URLROOT;?>/churchbudgets" class="nav-link">
+                        <p>Church Budget</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="<?php echo URLROOT;?>/groupbudgets" class="nav-link">
+                        <p>Group Budget</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="<?php echo URLROOT;?>/journals" class="nav-link">
+                        <p>Journal Entry</p>
+                    </a>
+                </li>
+                <li class="nav-item">
                     <a href="<?php echo URLROOT;?>/invoices" class="nav-link">
                         <p>Customer Invoices</p>
                     </a>
@@ -56,13 +102,13 @@
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a href="<?php echo URLROOT;?>/bankbalances" class="nav-link">
-                        <p>Bank Balances</p>
+                    <a href="<?php echo URLROOT;?>/deposits" class="nav-link">
+                        <p>Deposits </p>
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a href="<?php echo URLROOT;?>/bankpostings" class="nav-link">
-                        <p>Post Bankings</p>
+                    <a href="<?php echo URLROOT;?>/payments" class="nav-link">
+                        <p>Payments</p>
                     </a>
                 </li>
                 <li class="nav-item">
@@ -75,7 +121,7 @@
                         <p>Bank Reconcilliation</p>
                     </a>
                 </li>
-          </ul>
+            </ul>
         </li>
         <li class="nav-item">
             <a href="#" class="nav-link">
@@ -102,8 +148,18 @@
                     </a>
                 </li>
                 <li class="nav-item">
+                    <a href="<?php echo URLROOT;?>/reports/pettycash" class="nav-link">
+                        <p>Petty Cash Utilization</p>
+                    </a>
+                </li>
+                <li class="nav-item">
                     <a href="<?php echo URLROOT;?>/reports/budgetvsexpense" class="nav-link">
                         <p>Budget Vs Expense Reports</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="<?php echo URLROOT;?>/invoicereports" class="nav-link">
+                        <p>Invoice Reports</p>
                     </a>
                 </li>
                 <li class="nav-item">
@@ -117,7 +173,7 @@
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a href="<?php echo URLROOT;?>/reports/trialbalance" class="nav-link">
+                    <a href="<?php echo URLROOT;?>/trialbalance" class="nav-link">
                         <p>Trial Balance</p>
                     </a>
                 </li>
