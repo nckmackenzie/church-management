921e22617d16f84ba5490fa1f7e38bed75673eed
nckmackenzie <nicholas.mackenzie1@gmail.com>
2022-10-25 18:55:56 +0300

    create table

--- public/dist/js/pages/reports/tb/tb.js
+++ public/dist/js/pages/reports/tb/tb.js
@@ -12,14 +12,14 @@ import {
   createSpinnerContainer,
 } from '../utils.js';
 import { getTrialBalance } from '../ajax.js';
-const table = document.getElementById('table');
 
 //click handler
 btnPreview.addEventListener('click', async function () {
   if (validation() > 0) return;
   if (!validateDate(sdateInput, edateInput)) return;
   resultsDiv.classList.add('d-none');
-  table.getElementsByTagName('tbody')[0].innerHTML = '';
+  resultsDiv.innerHTML = '';
+  // table.getElementsByTagName('tbody')[0].innerHTML = '';
   setLoadingSpinner();
   const sdateVal = sdateInput.value;
   const edateVal = edateInput.value;
@@ -27,7 +27,8 @@ btnPreview.addEventListener('click', async function () {
   const data = await getTrialBalance(reportType, sdateVal, edateVal);
   removeLoadingSpinner(resultsDiv);
   if (data && data.success) {
-    appendTbody(data.results, reportType);
+    resultsDiv.innerHTML = appendTbody(data.results, reportType);
+    const table = document.getElementById('table');
     const debitTotal = document.getElementById('debittotal');
     const creditTotal = document.getElementById('credittotal');
     debitTotal.innerText = getColumnTotal(table, 1);
@@ -37,10 +38,19 @@ btnPreview.addEventListener('click', async function () {
 });
 
 function appendTbody(data, type) {
-  const tbody = table.getElementsByTagName('tbody')[0];
+  let html = `
+  <table id="table" class="table table-striped table-bordered table-sm">
+    <thead class="bg-lightblue">
+      <tr>
+          <th>Account</th>
+          <th class="text-center">Debit</th>
+          <th class="text-center">Credit</th>
+      </tr>
+    </thead>
+    <tbody>`;
   data.forEach(dt => {
     if (dt.credit !== '' || dt.Debit !== '') {
-      let html = `
+      html += `
         <tr>
             <td>${String(
               type === 'detailed' ? dt.account : dt.parentaccount
@@ -51,11 +61,22 @@ function appendTbody(data, type) {
                 ? ''
                 : numberWithCommas(parseFloat(dt.credit).toFixed(2))
             }</td>
-        </tr>
-    `;
-      tbody.insertAdjacentHTML('beforeend', html);
+        </tr>`;
     }
   });
+  html += `
+  </tbody>
+  <tfoot>
+      <tr>
+          <th style="text-align:right">Total</th>
+          <th class="text-center" id="debittotal"></th>
+          <th class="text-center" id="credittotal"></th>
+      </tr>
+  </tfoot>
+</table>
+  `;
+
+  return html;
 }
 
 clearOnChange(mandatoryFields);
