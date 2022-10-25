498d41059d01f3650c86908806af6d02fc4cedae
nckmackenzie <nicholas.mackenzie1@gmail.com>
2022-10-25 22:29:43 +0300

    remove title

--- app/views/reports/trialbalancereport.php
+++ app/views/reports/trialbalancereport.php
@@ -8,7 +8,7 @@
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h6>Blank Page</h6>
+            <h6></h6>
           </div>
           <div class="col-sm-6"></div>
         </div>
