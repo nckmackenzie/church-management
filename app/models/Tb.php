823372489302578b828b7585c43aa7b7222d3a83
nckmackenzie <nicholas.mackenzie1@gmail.com>
2022-10-25 22:12:46 +0300

    fetch trial balance detailed

--- app/models/Tb.php
+++ app/models/Tb.php
@@ -16,4 +16,24 @@ class Tb
             return loadresultset($this->db->dbh,'CALL sp_trialbalance_summary(?,?,?)',[$data['sdate'],$data['edate'],$_SESSION['congId']]);
         }
     }
+
+    public function GetDetailedTbReport($data)
+    {
+        if($data['type'] !== 'summary' && $data['type'] !== 'detailed'){
+            return false;
+        }
+        $sql = '';
+        if($data['type'] === 'summary'){
+            $sql = 'SELECT transactionDate,account,debit,credit,narration,t.TransactionType 
+                    FROM tblledger l left join tbltransactiontypes t on l.transactionType = t.ID 
+                    WHERE (parentaccount = ?) AND (transactionDate BETWEEN ? AND ?) AND (l.deleted = 0)
+                    ORDER BY transactionDate';
+        }elseif($data['type'] === 'detailed'){
+            $sql = 'SELECT transactionDate,account,debit,credit,narration,t.TransactionType 
+                    FROM tblledger l left join tbltransactiontypes t on l.transactionType = t.ID 
+                    WHERE (account = ?) AND (transactionDate BETWEEN ? AND ?) AND (l.deleted = 0)
+                    ORDER BY transactionDate';
+        }
+        return loadresultset($this->db->dbh,$sql,[$data['account'],$data['sdate'],$data['edate']]);
+    }
 }
