0221e986c318561100bfdc9eed287558e0c836bb
ncmackenzie <34639621+nckmackenzie@users.noreply.github.com>
2022-11-08 08:17:54 +0300

    add Bulk SMS variables

--- app/config/config.php
+++ app/config/config.php
@@ -11,3 +11,7 @@ define('URLROOT','http://localhost/cms');
 //SITE NAME
 define('SITENAME','Kenyatta Rd Parish');
 define('ENVIRONMENT','production');
+//SMS API VARS
+define('USER_SMS','jujafarmcms');
+define('APIKEY','f7a976d9fb97abe4f784a34a8b201286e6fa0adb10ace60269e0a81f5d564b53');
+define('SENDER','PCEAJUJAFAR');
