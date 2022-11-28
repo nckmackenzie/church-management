dd70e61c439a61da593dea195e5ff0fa51e61221
nckmackenzie <nicholas.mackenzie1@gmail.com>
2022-11-28 10:27:27 +0300

    remove account id encrypting

--- app/views/accounts/index.php
+++ app/views/accounts/index.php
@@ -1,6 +1,34 @@
 <?php require APPROOT . '/views/inc/header.php';?>
 <?php require APPROOT . '/views/inc/topNav.php';?>
 <?php require APPROOT . '/views/inc/sideNav.php';?>
+<!-- Modal -->
+<div class="modal fade" id="deleteModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLongTitle">Delete Account</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+          <form action="<?php echo URLROOT;?>/accounts/delete" method="post">
+              <div class="row">
+                <div class="col-md-9">
+                  <label for="">Delete Selected Account?</label>
+                  <input type="hidden" name="id" id="id">
+                </div>
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="submit" class="btn btn-danger">Yes</button>
+              </div>
+          </form>
+      </div>
+     
+    </div>
+  </div>
+</div>
   <!-- Content Wrapper. Contains page content -->
   <div class="content-wrapper">
     <!-- Content Header (Page header) -->
@@ -38,7 +66,8 @@
                                 <?php if($_SESSION['userType'] <=2) : ?>
                                   <td>
                                      <?php if($account->isEditable == 1) : ?>
-                                        <a href="<?php echo URLROOT;?>/accounts/edit/<?php echo encryptId($account->ID);?>" class="btn btn-sm bg-olive custom-font">Edit</a>
+                                        <a href="<?php echo URLROOT;?>/accounts/edit/<?php echo $account->ID;?>" class="btn btn-sm bg-olive custom-font">Edit</a>
+                                        
                                     <?php endif;?>
                                   </td>     
                                 <?php endif; ?>
@@ -63,7 +92,8 @@
                                       <td class="sub-level-3"><?php echo $child->accountType;?></td>
                                       <td><?php echo $child->atype;?></td>
                                       <td>
-                                          <a href="<?php echo URLROOT;?>/accounts/edit/<?php echo encryptId($child->ID);?>" class="btn btn-sm bg-olive custom-font">Edit</a>
+                                          <a href="<?php echo URLROOT;?>/accounts/edit/<?php echo $child->ID;?>" class="btn btn-sm bg-olive custom-font">Edit</a>
+                                          <button type="button" class="btn btn-sm btn-danger custom-font btndel">Delete</button>
                                       </td>
                                   </tr>
                                 <?php endforeach; ?>
@@ -86,9 +116,22 @@
             {"visible" : false, "targets": 0},
             {"width" : "15%" , "targets": 2}
             <?php if ($_SESSION['userType'] <=2) : ?>
-            ,{"width" : "10%" , "targets": 3},
+            ,{"width" : "15%" , "targets": 3},
             <?php endif;?>
           ]
+      });
+
+      $('#accountsTable').on('click','.btndel',function(){
+          $('#deleteModalCenter').modal('show');
+          $tr = $(this).closest('tr');
+
+          let data = $tr.children('td').map(function(){
+              return $(this).text();
+          }).get();
+          
+          var currentRow = $(this).closest("tr");
+          var data1 = $('#accountsTable').DataTable().row(currentRow).data();
+          $('#id').val(data1[0]);
       });
     });
 </script>
