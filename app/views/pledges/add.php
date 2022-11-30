49d598d0a083313fae58ae52df35a3bcc50f4273
nckmackenzie <nicholas.mackenzie1@gmail.com>
2022-11-30 21:43:57 +0300

    refactoring of create pledge frontend

--- app/views/pledges/add.php
+++ app/views/pledges/add.php
@@ -16,34 +16,32 @@
     <!-- Main content -->
     <section class="content">
         <div class="row">
+            <div class="col-12" id="alertBox"></div>
             <div class="col-md-9 mx-auto">
                 <div class="card bg-light">
                     <div class="card-header">Add Pledge</div>
                     <div class="card-body">
-                        <form action="<?php echo URLROOT;?>/pledges/create" method="post">
+                        <form id="pledge-form" method="post">
                             <div class="row">
                                 <div class="col-md-6">
                                     <div class="form-group">
                                         <label for="category">Category</label>
-                                        <select name="category" id="category" class="form-control form-control-sm">
+                                        <select name="category" id="category" 
+                                                class="form-control form-control-sm mandatory">
+                                            <option value="" selected disabled>Select category</option>
                                             <option value="1"<?php selectdCheck($data['category'],1)?>>Member</option>
                                             <option value="2"<?php selectdCheck($data['category'],2)?>>Group</option>
                                             <option value="3"<?php selectdCheck($data['category'],3)?>>District</option>
                                         </select>
+                                        <span class="invalid-feedback"><?php echo $data['category_err'];?></span>
                                     </div>
                                 </div>
                                 <div class="col-md-6">
                                     <div class="form-group">
                                         <label for="pledger">Pledged By</label>
                                         <select name="pledger" id="pledger"
-                                                class="select2 form-control form-control-sm">
-                                            <?php foreach($data['pledgers'] as $pledger) : ?>
-                                                <option value="<?php echo $pledger->ID;?>"
-                                                <?php selectdCheck($data['pledger'],$pledger->ID)?>>
-                                                    <?php echo $pledger->pledger;?>
-                                                </option>
-                                            <?php endforeach;?>    
-                                        </select>
+                                                class="select2 form-control form-control-sm mandatory"></select>
+                                        <span class="invalid-feedback"></span>
                                     </div>
                                 </div>
                             </div><!--End Of Row-->
@@ -86,7 +84,8 @@
                                     <div class="form-group">
                                         <label for="paymethod">Payment Method</label>
                                         <select name="paymethod" id="paymethod" 
-                                                class="form-control form-control-sm mandatory">
+                                                class="form-control form-control-sm">
+                                            <option value="" selected disabled>Select pay method</option>
                                             <?php foreach($data['paymethods'] as $paymethod) : ?>
                                                 <option value="<?php echo $paymethod->ID;?>"
                                                 <?php selectdCheck($data['paymethod'],$paymethod->ID)?>>
@@ -94,6 +93,7 @@
                                                 </option>
                                             <?php endforeach; ?>
                                         </select>
+                                        <span class="invalid-feedback"></span>
                                     </div>
                                 </div>
                             </div><!--End Of Row-->
@@ -103,6 +103,7 @@
                                         <label for="bank">Bank</label>
                                         <select name="bank" id="bank" class="form-control form-control-sm
                                         <?php echo (!empty($data['bank_err'])) ? 'is-invalid' : ''?>">
+                                            <option value="" selected disabled>Select bank</option>
                                             <?php foreach($data['banks'] as $bank) : ?>
                                                 <option value="<?php echo $bank->ID;?>"
                                                     <?php selectdCheck($data['bank'],$bank->ID)?>>
@@ -128,7 +129,7 @@
                             </div><!--End Of Row-->
                             <div class="row">
                                 <div class="col-4">
-                                    <button type="submit" class="btn btn-sm bg-navy custom-font">Save</button>
+                                    <button type="submit" class="btn btn-sm bg-navy custom-font save">Save</button>
                                     <input type="hidden" name="pledgername" id="pledgername">
                                 </div>
                             </div>
@@ -140,86 +141,10 @@
     </section><!-- /.content -->
 </div><!-- /.content-wrapper -->
 <?php require APPROOT . '/views/inc/footer.php'?>
+<script type="module" src="<?php echo URLROOT;?>/dist/js/pages/pledges/add.js"></script>
 <script>
     $(function(){
         $('.select2').select2();
-
-        $(window).on('load',function(){
-            var now = new Date();
-            var day = ("0" + now.getDate()).slice(-2);
-            var month = ("0" + (now.getMonth() + 1)).slice(-2);
-            var today = now.getFullYear()+"-"+(month)+"-"+(day) ;
-            $('#date').val(today);
-            $('#bank').val('');
-            $('#bank').attr('disabled',true);
-            if ($('#amountpaid').val() > 0) {
-                $('#paymethod').attr('disabled',false);
-                $('#reference').attr('disabled',false);
-            }
-            else{
-                $('#paymethod').attr('disabled',true);
-                $('#paymethod').val('');
-                $('#reference').attr('disabled',true);
-                $('#reference').val('');
-            }
-            getPledgerName();
-        });
-
-        $('#paymethod').change(function(){
-            var paym = $(this).val();
-            if (paym >= 3) {
-                $('#bank').attr('disabled',false);
-                $('#bank').prop("selectedIndex", 0);
-                $('#reference').addClass('mandatory');
-            }
-            else if(paym == 2){
-                $('#reference').addClass('mandatory');
-                $('#bank').attr('disabled',true);
-                $('#bank').val('');
-            }
-            else{
-                $('#bank').attr('disabled',true);
-                $('#bank').val('');
-                $('#reference').removeClass('mandatory');
-            }
-        });
-        $('#category').change(function(){
-            var category = $(this).val();
-            $.ajax({
-                url : '<?php echo URLROOT;?>/pledges/getpledger',
-                method : 'POST',
-                data : {category : category},
-                success : function(data){
-                    $('#pledger').html(data);
-                }
-            });
-        });
-        $('#amountpaid').focusout(function(){
-            var amount = $(this).val();
-            if (amount > 0) {
-                $('#paymethod').attr('disabled',false);
-                $('#paymethod').prop("selectedIndex", 0);
-                $('#reference').attr('disabled',false);
-                $('#reference').val('');
-            }
-            else{
-                $('#bank').val('');
-                $('#bank').attr('disabled',true);
-                $('#paymethod').attr('disabled',true);
-                $('#paymethod').val('');
-                $('#reference').attr('disabled',true);
-                $('#reference').val('');
-            }
-            getPledgerName();
-        });
-        $('#pledger').change(function(){
-            getPledgerName();
-        });
-        function getPledgerName(){
-            var data=$('#pledger').select2('data');
-            var selectedText = (data[0].text).trim();
-            $('#pledgername').val(selectedText);
-        }
     });
 </script>
 </body>
