790ec17ff5fe99dcb923d837115de57318b41c25
nckmackenzie <nicholas.mackenzie1@gmail.com>
2022-12-03 15:53:54 +0300

    more elements definitions

--- public/dist/js/pages/journals/elements.js
+++ public/dist/js/pages/journals/elements.js
@@ -6,8 +6,16 @@ export const addBtn = document.querySelector('.add');
 export const saveBtn = document.querySelector('.save');
 //form elements
 export const form = document.querySelector('#journal-form');
-export const journalNo = document.querySelector('#journalno');
+export const journalNoInput = document.querySelector('#journalno');
+export const accountSlct = document.querySelector('#account');
+export const typeSlct = document.querySelector('#type');
+export const amountInput = document.querySelector('#amount');
+export const descInput = document.querySelector('#description');
+export const debitsInput = document.querySelector('#debits');
+export const creditsInput = document.querySelector('#credits');
 
 //other
 export const spinnerContainer = document.querySelector('.spinner-container');
 export const entries = document.querySelector('.entries');
+export const table = document.querySelector('#table-entries');
+export const tbody = table.getElementsByTagName('tbody')[0];
