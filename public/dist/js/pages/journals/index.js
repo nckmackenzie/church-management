6d306d017f049124f77419911cf55cba70e63534
nckmackenzie <nicholas.mackenzie1@gmail.com>
2022-12-03 15:52:48 +0300

    renaming and naming inputs and elements

--- public/dist/js/pages/journals/index.js
+++ public/dist/js/pages/journals/index.js
@@ -1,10 +1,13 @@
 import { removeLoadingSpinner, setLoadingSpinner } from '../reports/utils.js';
 import { getJournalNo } from './ajax.js';
-import { journalNo } from './elements.js';
+import { addBtn, journalNoInput } from './elements.js';
+import { addToTable } from './functionalities.js';
 
 async function reset() {
   const data = await getJournalNo();
-  if (data && data.success) journalNo.value = data.journalno;
+  if (data && data.success) journalNoInput.value = data.journalno;
 }
+
+addBtn.addEventListener('click', addToTable);
 
 reset();
