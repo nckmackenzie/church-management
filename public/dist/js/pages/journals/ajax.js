99d338b38f50b0ad7a097ba67e566f255ca5971d
nckmackenzie <nicholas.mackenzie1@gmail.com>
2022-12-04 17:10:16 +0300

    function to make a POST request to create entries

--- public/dist/js/pages/journals/ajax.js
+++ public/dist/js/pages/journals/ajax.js
@@ -1,6 +1,20 @@
-import { HOST_URL } from '../utils/utils.js';
+import { alertBox, HOST_URL, sendHttpRequest } from '../utils/utils.js';
 import { getRequest } from '../reports/utils.js';
 
 export async function getJournalNo() {
   return await getRequest(`${HOST_URL}/journals/getjournalno`);
+}
+
+export async function saveEntries(formData) {
+  const url = `${HOST_URL}/journals/createupdate`;
+  const res = await sendHttpRequest(
+    url,
+    'POST',
+    JSON.stringify(formData),
+    {
+      'Content-Type': 'application/json',
+    },
+    alertBox
+  );
+  return res;
 }
