ca1378ae6eba2dfaafab90e119871ceb401dd961
ncmackenzie <34639621+nckmackenzie@users.noreply.github.com>
2022-12-07 09:15:51 +0300

    get journal no based on delete and handle update of entries

--- app/models/Journal.php
+++ app/models/Journal.php
@@ -27,7 +27,7 @@ class Journal {
     {
        $this->db->query('SELECT COUNT(ID) 
                          FROM tblledger 
-                         WHERE (isJournal=1) AND (congregationId=:cid)');
+                         WHERE (isJournal=1) AND (congregationId=:cid) AND (deleted = 0)');
        $this->db->bind(':cid',$_SESSION['congId']);
        $result = $this->db->getValue();
        if ($result == 0) {
@@ -36,7 +36,7 @@ class Journal {
        else{
             $this->db->query('SELECT journalNo 
                               FROM tblledger 
-                              WHERE (isJournal = 1) AND (congregationId=:cid)
+                              WHERE (isJournal = 1) AND (congregationId=:cid) AND (deleted = 0)
                               ORDER BY journalNo DESC LIMIT 1');
             $this->db->bind(':cid',$_SESSION['congId']);
             return $this->db->getValue() + 1;
@@ -62,65 +62,19 @@ class Journal {
         if($type === 'current') return (int)$journalno + 1;
         return (int)$journalno;
     }
-    public function create($data)
-    {
-        try {
-            $five = 5;
-            $jno = $data['journal'];
-            $isj = 1;
-            $cong = $_SESSION['congId'];
-            $this->db->dbh->beginTransaction();
-            $sql = 'INSERT INTO tblledger (transactionDate,account,debit,credit,narration,accountId,
-                transactionType,transactionId,isJournal,journalNo,congregationId) 
-                VALUES(?,?,?,?,?,?,?,?,?,?,?)';
-            // $this->db->query('INSERT INTO tblledger (transactionDate,account,debit,credit,narration,
-            //                               accountId,transactionType,transactionId,isJournal,journalNo,
-            //                               congregationId)
-            //                   VALUES(:tdate,:acc,:debit,:credit,:narr,:aid,:ttype,:tid,:isj,:jno,:cid)');
-            for ($i=0; $i < count($data['details']); $i++) { 
-                $date =date($data['details'][$i]['date']);
-                $account =trim(strtolower($data['details'][$i]['account']));
-                $debit =!empty($data['details'][$i]['debit']) ? $data['details'][$i]['debit'] : 0;
-                $credit =!empty($data['details'][$i]['credit']) ? $data['details'][$i]['credit'] : 0;
-                $desc =trim(strtolower($data['details'][$i]['desc']));
-                $aid =$this->getAccountId(trim($data['details'][$i]['aid']));
-                
-                $stmt = $this->db->dbh->prepare($sql);
-                $stmt->execute([$date,$account,$debit,$credit,$desc,$aid,$five,$jno,$isj,$jno,$cong]);
-                // $this->db->bind(':tdate',$date);
-                // $this->db->bind(':acc',$account);
-                // $this->db->bind(':debit',$debit);
-                // $this->db->bind(':credit',$credit);
-                // $this->db->bind(':narr',$desc);
-                // $this->db->bind(':aid',$aid);
-                // $this->db->bind(':ttype',$five);
-                // $this->db->bind(':tid',$jno);
-                // $this->db->bind(':isj',$isj);
-                // $this->db->bind(':jno',$jno);
-                // $this->db->bind(':cid',$cong);
-                // $this->db->execute();
-            }    
-            
-            $act = 'Created Journal Entry '.$data['journal'];
-            saveLog($this->db->dbh,$act);
-            if ($this->db->dbh->commit()) {
-                return true;
-            }
-            else {
-                return false;
-            }
-        } catch (\Exception $e) {
-            if ($this->db->dbh->inTransaction()) {
-                $this->db->dbh->rollBack();
-            }
-            throw $e;
-        }
-    }
-
+   
     public function createupdate($data)
     {
         try {
             $this->db->dbh->beginTransaction();
+
+            if($data['isedit']){
+                $this->db->query('DELETE FROM tblledger 
+                                  WHERE (isJournal = 1) AND (deleted = 0) AND (journalNo=:jno) AND (congregationId = :cid)');
+                $this->db->bind(':jno',$data['journalno']);
+                $this->db->bind(':cid',$_SESSION['congId']);
+                $this->db->execute();
+            }
 
             for ($i=0; $i < count($data['entries']); $i++) { 
                 $account = strtolower(trim($data['entries'][$i]->accountname));
@@ -149,7 +103,8 @@ class Journal {
                 $this->db->execute();
             }
 
-            saveLog($this->db->dbh,'Made entries for journal no '.$data['journalno']);
+            $narr = $data['isedit'] ? 'Made changes to journal no '.$data['journalno'] : 'Made entries for journal no '.$data['journalno'];
+            saveLog($this->db->dbh,$narr);
 
             if(!$this->db->dbh->commit()){
                 return false;
