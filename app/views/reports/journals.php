60c08d39f3aa862192cfaba6388446bfc265f5ee
nicmackenzie <nicmackenzie@live.com>
2024-01-24 12:06:44 +0300

    feat: Page to view journal entries report

---
+++ app/views/reports/journals.php
@@ -0,0 +1,124 @@
+<?php require APPROOT . '/views/inc/header.php';?>
+<?php require APPROOT . '/views/inc/topNav.php';?>
+<?php require APPROOT . '/views/inc/sideNav.php';?>
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-md-3">
+            <label for="from">From</label>
+            <input type="date" name="from" id="from" class="form-control form-control-sm">
+            <span class="invalid-feedback" id="from_err"></span>
+          </div>
+          <div class="col-md-3">
+            <label for="to">To</label>
+            <input type="date" name="to" id="to" class="form-control form-control-sm">
+            <span class="invalid-feedback" id="to_err"></span>
+          </div>
+          
+        </div>
+        <div class="row">
+            <div class="col-md-6 mt-2">
+                <button class="btn btn-sm bg-navy custom-font" id="preview">Preview</button>
+            </div>
+        </div>
+      </div><!-- /.container-fluid -->
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+        <div class="row">
+            <div class="col-md-12">
+                <div id="results" class="table-responsive">
+
+                </div>
+            </div>
+        </div>               
+    </section><!-- /.content -->
+</div><!-- /.content-wrapper -->
+<?php require APPROOT . '/views/inc/footer.php'?>
+<script>
+    $(function(){
+        $('#preview').click(function(){
+            var table = $('#table').DataTable();
+            //validate
+            var from_err = '';
+            var to_err = '';
+          
+            if($('#from').val() == ''){
+                from_err = 'Select Start Date';
+                $('#from_err').text(from_err);
+                $('#from').addClass('is-invalid');
+            }else{
+                from_err = '';
+                $('#from_err').text(from_err);
+                $('#from').removeClass('is-invalid');
+            }
+
+            if($('#to').val() == ''){
+                to_err = 'Select End Date';
+                $('#to_err').text(to_err);
+                $('#to').addClass('is-invalid');
+            }else{
+                to_err = '';
+                $('#to_err').text(to_err);
+                $('#to').removeClass('is-invalid');
+            }
+
+            if(from_err !== '' || to_err !== '') return;
+            
+            var from = $('#from').val();
+            var to = $('#to').val();
+    
+            $.ajax({
+                url : '<?php echo URLROOT;?>/reports/journalsrpt',
+                method : 'GET',
+                data : {from : from, to : to},
+                success : function(data){
+                    // console.log(data);
+                    $('#results').html(data);
+                    table.destroy();
+                    table = $('#table').DataTable({
+                        pageLength : 100,
+                        fixedHeader : true,
+                        ordering : false,
+                        "responsive" : true,
+                        "buttons": ["excel", "pdf","print"],
+                        "footerCallback": function ( row, data, start, end, display ) {
+                            var api = this.api(), data;
+                             // Remove the formatting to get integer data for summation
+                            var intVal = function ( i ) {
+                                return typeof i === 'string' ?
+                                    i.replace(/[\$,]/g, '')*1 :
+                                    typeof i === 'number' ?
+                                        i : 0;
+                            };
+
+                            function updateValues(cl){
+                                total = api
+                                      .column( cl )
+                                      .data()
+                                      .reduce( function (a, b) {
+                                      return intVal(a) + intVal(b);
+                                      },0);
+                                return total;      
+                            }
+
+                            function format_number(n) {
+                              return n.toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, "$1,");
+                            }
+                            // Update footer
+                            $('#debits').html(format_number(updateValues(3)));
+                            $('#credits').html(format_number(updateValues(4)));
+                            
+                        }
+                    }).buttons().container().appendTo('#table_wrapper .col-md-6:eq(0)');
+                }
+            });
+        });
+    });
+</script>
+</body>
+</html>  
