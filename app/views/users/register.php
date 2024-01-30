bef83843a3906cef887e2957ae83160bd9fcbd91
nicmackenzie <nicmackenzie@live.com>
2024-01-30 15:57:16 +0300

    feat: users add and edit in one area

--- app/views/users/register.php
+++ app/views/users/register.php
@@ -3,8 +3,39 @@
 <?php require APPROOT . '/views/inc/sideNav.php';?>
 
 <div class="content-wrapper">
+    <section class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+             <a href="<?php echo URLROOT;?>/users/all" class="btn btn-dark btn-sm mt-2"><i class="fas fa-backward"></i> Back</a>
+          </div>
+        </div>
+      </div><!-- /.container-fluid -->
+    </section>
     <!-- Main content -->
     <section class="content">
+        <div class="row">
+            <div class="col-sm-12" id="alertBox">
+                <?php if(count($data['errors']) > 0) : ?>
+                    <div class="alert custom-danger">
+                        <?php foreach($data['errors'] as $error) : ?>
+                            <div><?php echo $error;?></div>
+                        <?php endforeach; ?>
+                    </div>
+                <?php endif; ?>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-6 mx-auto d-none" id="success-msg">
+                <div class="alert custom-success alert-dismissible fade show mt-2">
+                    <span class="message">asdsad</span>
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                
+            </div>
+        </div>
         <div class="row">
             <div class="col-md-6 mx-auto">
                 <div class="card card-body bg-light mt-5">
@@ -16,20 +47,18 @@
                                 <div class="form-group">
                                     <label for="userid">UserID</label>
                                     <input type="text" name="userid"
-                                    class="form-control form-control-sm mandatory
-                                    <?php echo (!empty($data['userid_err'])) ? 'is-invalid' : ''?>"
-                                    value="<?php echo $data['userid'];?>" autocomplete="off">
-                                    <span class="invalid-feedback"><?php echo $data['userid_err'];?></span>
+                                    class="form-control form-control-sm mandatory"
+                                    value="<?php echo $data['userid'];?>" autocomplete="off" placeholder="jdoe">
+                                    <span class="invalid-feedback"></span>
                                 </div>
                             </div>
                             <div class="col-md-6">
                                 <div class="form-group">
                                     <label for="username">User Name</label>
                                     <input type="text" name="username"
-                                    class="form-control form-control-sm mandatory
-                                    <?php echo (!empty($data['username_err'])) ? 'is-invalid' : ''?>"
-                                    value="<?php echo $data['username'];?>" autocomplete="off">
-                                    <span class="invalid-feedback"><?php echo $data['username_err'];?></span>
+                                    class="form-control form-control-sm mandatory"
+                                    value="<?php echo $data['username'];?>" autocomplete="off" placeholder="Jane Doe">
+                                    <span class="invalid-feedback"></span>
                                 </div>
                             </div>
                         </div><!--End Of Row -->
@@ -39,10 +68,10 @@
                                     <label for="usertype">User Type</label>
                                     <select name="usertype" id="usertype" 
                                             class="form-control form-control-sm">
-                                        <option value="2">ADMINISTRATOR</option>    
-                                        <option value="3" selected>STANDARD USER</option>    
-                                        <option value="4">ELDER</option>    
-                                        <option value="5">ACCOUNTS ADMIN</option>    
+                                        <option value="2" <?php selectdCheck($data['usertype'],"2") ?>>ADMINISTRATOR</option>    
+                                        <option value="3" <?php selectdCheck($data['usertype'],"3") ?>>STANDARD USER</option>    
+                                        <option value="4" <?php selectdCheck($data['usertype'],"4") ?>>ELDER</option>    
+                                        <option value="5" <?php selectdCheck($data['usertype'],"5") ?>>ACCOUNTS ADMIN</option>    
                                     </select>
                                 </div>
                             </div>
@@ -51,6 +80,7 @@
                                 <label for="district">District</label>
                                     <select name="district" id="district" 
                                             class="form-control form-control-sm" disabled>
+                                            <option value="" selected disabled>Select district</option>
                                         <?php foreach($data['districts'] as $district) : ?>
                                             <option value="<?php echo $district->ID;?>"
                                             <?php selectdCheck($data['district'],$district->ID)?>>
@@ -58,7 +88,7 @@
                                             </option>
                                         <?php endforeach; ?>
                                     </select>
-                                    <span class="invalid-feedback"><?php echo $data['district_err'];?></span>
+                                    <span class="invalid-feedback"></span>
                                 </div>
                             </div>
                         </div><!--End Of Row -->
@@ -67,10 +97,10 @@
                                 <div class="form-group">
                                     <label for="contact">Contact</label>
                                     <input type="text" name="contact"
-                                    class="form-control form-control-sm mandatory
-                                    <?php echo (!empty($data['contact_err'])) ? 'is-invalid' : ''?>"
-                                    value="<?php echo $data['contact'];?>" maxlength="10" autocomplete="off">
-                                    <span class="invalid-feedback"><?php echo $data['contact_err'];?></span>
+                                    class="form-control form-control-sm mandatory"
+                                    value="<?php echo $data['contact'];?>" maxlength="10" 
+                                    autocomplete="off" placeholder="0700000000">
+                                    <span class="invalid-feedback"></span>
                                 </div>
                             </div>
                             <div class="col-md-6">
@@ -78,15 +108,17 @@
                                     <label for="active">Status</label>
                                     <select name="active" id="active" 
                                             class="form-control form-control-sm">
-                                        <option value="1">Active</option>    
-                                        <option value="0">Inactive</option>    
+                                        <option value="1" <?php selectdCheck($data['active'],"1") ?>>Active</option>    
+                                        <option value="0" <?php selectdCheck($data['active'],"0") ?>>Inactive</option>    
                                     </select>
                                 </div>
                             </div>
                         </div><!--End Of Row -->
                         <div class="row">
                             <div class="col-2">
-                            <button type="submit" class="btn btn-block btn-sm bg-navy">Save</button>
+                                <button   button type="submit" class="btn btn-block btn-sm bg-navy">Save</button>
+                                <input type="hidden" id="id" name="id" value="<?php echo $data['id'];?>"  >
+                                <input type="hidden" id="isedit" name="isedit" value="<?php echo $data['isedit'];?>"  >
                             </div>
                         </div>
                     </form>
@@ -98,22 +130,7 @@
   </div>
   <!-- /.content-wrapper -->
 <?php require APPROOT . '/views/inc/footer.php'?>
-<script>
-    $(function(){
-        $(window).on('load',function(){
-            $('#district').val('');
-        });
+<script type="module" src="<?php echo URLROOT;?>/dist/js/pages/users/create.js"></script>
 
-        $('#usertype').change(function() {
-            if ($(this).val() == 4) {
-                $('#district').attr('disabled',false);
-                $('#district').prop("selectedIndex", 0);
-            }else{
-                $('#district').attr('disabled',true);
-                $('#district').val('');
-            }
-        });
-    });
-</script>
 </body>
 </html>
