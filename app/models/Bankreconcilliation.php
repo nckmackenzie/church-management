9eb691e18e7294c68fe85724a49a70cc0f854dda
nckmackenzie <nicholas.mackenzie1@gmail.com>
2024-09-02 21:37:00 +0300

    refactor: change opening balance reading point

--- app/models/Bankreconcilliation.php
+++ app/models/Bankreconcilliation.php
@@ -105,6 +105,87 @@ class Bankreconcilliation
         return $amounts;
     }
 
+    public function getAmountsrecon($data)
+    {
+        $amounts = [];
+        $deposits = 0;
+        $withdrawals = 0;
+        $unclearedDeposits = 0;
+        $unclearedWithdrawals = 0;
+
+        $this->db->query('SELECT IFNULL(SUM(debit),0) As SumOfDebits
+                          FROM   tblbankpostings
+                          WHERE  (transactionDate BETWEEN :tfrom AND :tto) AND (cleared=1) 
+                                 AND (deleted=0) AND (bankId=:bid) AND (congregationId=:cid) AND (clearedDare <= :tdate)');
+        $this->db->bind(':tfrom',$data['from']);
+        $this->db->bind(':tto',$data['to']);
+        $this->db->bind(':bid',$data['bank']);
+        $this->db->bind(':cid',$_SESSION['congId']);
+        $this->db->bind(':tdate',$data['to']);
+        $deposits = $this->db->getValue();
+        array_push($amounts,$deposits);
+
+        $this->db->query('SELECT IFNULL(SUM(credit),0) As SumOfCredits
+                          FROM   tblbankpostings
+                          WHERE  (transactionDate BETWEEN :tfrom AND :tto) AND (cleared=1) 
+                                 AND (deleted=0) AND (bankId=:bid) AND (congregationId=:cid) AND (clearedDare <= :tdate)');
+        $this->db->bind(':tfrom',$data['from']);
+        $this->db->bind(':tto',$data['to']);
+        $this->db->bind(':bid',$data['bank']);
+        $this->db->bind(':cid',$_SESSION['congId']);
+        $this->db->bind(':tdate',$data['to']);
+        $withdrawals = $this->db->getValue();
+        array_push($amounts,$withdrawals);
+
+        $this->db->query('SELECT IFNULL(SUM(debit),0) As SumOfDebits
+                          FROM   tblbankpostings
+                          WHERE  ((transactionDate BETWEEN :tfrom AND :tto) AND (cleared=0) 
+                                 AND (deleted=0) AND (bankId=:bid) AND (congregationId=:cid)) 
+                                 OR (cleared=1 AND clearedDare > :tdate AND (transactionDate BETWEEN :tfrom AND :tto) 
+                                 AND (deleted=0) AND (bankId=:bid) AND (congregationId=:cid))');
+        $this->db->bind(':tfrom',$data['from']);
+        $this->db->bind(':tto',$data['to']);
+        $this->db->bind(':bid',$data['bank']);
+        $this->db->bind(':cid',$_SESSION['congId']);
+        $this->db->bind(':tdate',$data['to']);
+        $unclearedDeposits = $this->db->getValue();
+        array_push($amounts,$unclearedDeposits);
+
+        $this->db->query('SELECT IFNULL(SUM(credit),0) As SumOfCredits
+                          FROM   tblbankpostings
+                          WHERE  ((transactionDate BETWEEN :tfrom AND :tto) AND (cleared=0) 
+                                 AND (deleted=0) AND (bankId=:bid) AND (congregationId=:cid)) OR 
+                                 (cleared=1 AND clearedDare > :tdate AND (transactionDate BETWEEN :tfrom AND :tto)
+                                 AND (deleted=0) AND (bankId=:bid) AND (congregationId=:cid))');
+        $this->db->bind(':tfrom',$data['from']);
+        $this->db->bind(':tto',$data['to']);
+        $this->db->bind(':bid',$data['bank']);
+        $this->db->bind(':cid',$_SESSION['congId']);
+        $this->db->bind(':tdate',$data['to']);
+        $unclearedWithdrawals = $this->db->getValue();
+        array_push($amounts,$unclearedWithdrawals);
+
+        $asofdate = $data['to'];
+
+        $this->db->query('CALL sp_balancesheet_assets(:startd,:cong)');
+        // $this->db->bind(':startd',$data['from']);
+        $this->db->bind(':startd',$asofdate);
+        $this->db->bind(':cong',$_SESSION['congId']);
+        $results = $this->db->resultSet();
+        foreach($results as $result){
+            if(strtolower($result->account) === 'cash at bank'){
+                array_push($amounts,$result->bal);
+            }
+        }
+        // $this->db->query($sql);
+        // $this->db->bind(':account','cash at bank');
+        // $this->db->bind(':asatdate',$data['from']);
+        // $this->db->bind(':cid',$_SESSION['congId']);
+        // $result = $this->db->single();
+        // array_push($amounts,$result->bal);
+        return $amounts;
+    }
+
     public function UnclearedReport($data)
     {
         if($data['type'] === 'withdraw'){
